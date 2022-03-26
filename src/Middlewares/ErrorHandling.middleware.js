2a065d8f492ea3864101fd7d177abfa3afbea6ae
Mostafa Magdy <mostafa.magdy621@gmail.com>
2022-03-26 22:23:15 +0200

    cors handling

--- src/Middlewares/ErrorHandling.middleware.js
+++ src/Middlewares/ErrorHandling.middleware.js
@@ -1,9 +1,13 @@
 const ApiError = require('../Helpers/ApiError')
 
 module.exports.handleError=(err,req,res,next)=>{
+    console.log(err.message)
+
     if(err instanceof ApiError)
     {
+
         res.status(err.code).json(err.message);
     }
+    res.json({status:"failed",message:err.message})
 next();
 }
--- src/Server.js
+++ src/Server.js
@@ -4,7 +4,12 @@ var morgan = require('morgan')
 const{handleError}=require('./Middlewares/ErrorHandling.middleware')
 const app=express()
 
-app.use(cors())
+
+var corsOptions = {
+    origin: 'http://localhost:4200',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+app.use(cors(corsOptions))
 require('express-async-errors');
 
 app.use(morgan('tiny'))
